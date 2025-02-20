ccd77e0a3863cb5b0cc12030ebfbaec54ac928a2
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-20 14:24:17 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,55 @@
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+	static String pyramid(int height){
+		String output = "";
+		for(int row = 0; row < height; ++row){
+			for(int spaces = 0; spaces < height-row-1; ++spaces){
+				output += " ";
+			}
+			for(int asterisks = 0; asterisks < row+1; ++asterisks){
+				output += "*";
+				if(asterisks != row){
+					output += " ";
+				}
+			}
+			if(row != height-1){
+				output += "\n";
+			}
+		}
+		return output;
 	}
 
+	static String square(int height){
+		String output = "";
+		for(int row = 0; row < height; ++row){
+			if(row == 0 || row == height-1){
+				for(int asterisks = 0; asterisks < height; ++asterisks){
+					output += "*";
+				}
+			}else{
+				output += "*";
+				for(int spaces = 1; spaces < height-1; ++spaces){
+						output += " ";
+				}
+				output += "*";
+			}
+			if(row != height-1){
+				output += "\n";
+			}
+		}
+		return output;
+	}
+
+	static boolean hasLowercase(String str){
+		for(int index = 0; index < str.length(); ++index){
+			int charCode = (int)str.charAt(index);
+			if(charCode >= 97 && charCode <= 122){
+				return true;
+			}
+		}
+		return false;
+		/* Alternatively
+		return str.toUpperCase() != str; */
+	}
+
+	public static void main(String args[]) {}
 }
